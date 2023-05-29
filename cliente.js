1661f3a906b512e664eb56a0cbacc8fc7308c500
Tavo0905 <gperezb2002@gmail.com>
2023-05-29 10:12:45 -0600

    Arreglo boton comentarios

--- cliente.js
+++ cliente.js
@@ -1185,14 +1185,14 @@ app.post("/comentario", urlParser, (req, res) => {
    }
 })
 
-app.post("/comentarios", urlParser, (req, res) => {
-   if (req.body.rdBtnComentario != "") {
-      const codigo = JSON.stringify({
-         user: usuario.user,
-         reply: req.body.rdBtnComentario,
-         mensaje: req.body.mensaje
-      });
-   }
+app.post("/comentarios", urlParser, upload.any(), (req, res) => {
+   const codigo = JSON.stringify({
+      user: usuario.user,
+      reply: req.body.rdBtnComentario,
+      mensaje: req.body.mensaje
+   });
+   
+   console.log(codigo)
 
    const options = {
       hostname: 'localhost',
